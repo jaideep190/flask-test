7eb17a88aad9b1a8161f5d45d203ec81d0a1f2be
jaideep190 <ajaiajai710@gmail.com>
2024-08-07 10:42:25 +0530

    Pinger Added

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
+from flask import request
+import requests
+import time
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def start():
+    while True:
+        response = requests.get("https://smartbp-backend.onrender.com/")
+        time.sleep(30)
     return "The flask Server is Running"
+
 
 @app.route("/index")
 def mbsa():
+    while True:
+        response = requests.get("https://smartbp-backend.onrender.com/")
+        time.sleep(30)
     return render_template('index.html')
 
